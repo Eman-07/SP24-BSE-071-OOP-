f83850c7926fcfaf964db19c1015bb478a96e08b
Muhammad Eman <memanmaan44@gmail.com>
2024-10-19 16:25:50 +0500

    Performed Lab6 Tasks (Practiced Inheritance)

--- lab6/Circle.java
+++ lab6/Circle.java
@@ -8,4 +8,9 @@ public class Circle extends Shape2d{
     public void circumference() {
         System.out.println("Circle circumference : " + 2*Math.PI*radius);
     }
+
+    @Override
+    public void area() {
+        System.out.println("Circle area :" + Math.PI*radius*radius);
+    }
 }
--- lab6/Main.java
+++ lab6/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Shape s1 = new Circle(5);
-
         s1.circumference();
+        s1.area();
     }
 }
